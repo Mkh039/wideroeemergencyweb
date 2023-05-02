bb0268e92418f5be4c436715b0a438dc0147fa4f
Shahzeb Khan <mkh039@post.uit.no>
2023-05-02 19:39:21 +0200

    Deploy to github

---
+++ assets/Home-375fcf5b.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d9ce6893.js";import{C as e}from"./CardLayout-49bcb34b.js";const n=r=>{const{navHeader:o,children:a}=r;return t(e,{navHeader:o,children:a})};export{n as default};
---
+++ assets/Test_HRK_controlpanel-5cd47fb1.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-d9ce6893.js";import{C as e}from"./CardLayout-49bcb34b.js";const c=r=>{const{navHeader:o,children:t}=r;return a(e,{navHeader:o,children:t})};export{c as default};
