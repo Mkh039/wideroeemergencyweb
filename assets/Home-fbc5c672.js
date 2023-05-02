31ba80a5aa0c15ffb65964c2f29f374c300516da
Shahzeb Khan <mkh039@post.uit.no>
2023-05-02 19:59:06 +0200

    deploy

---
+++ assets/Home-fbc5c672.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-6ec18f23.js";import{C as e}from"./CardLayout-d2005797.js";const n=r=>{const{navHeader:o,children:a}=r;return t(e,{navHeader:o,children:a})};export{n as default};
---
+++ assets/Test_HRK_controlpanel-93ebc093.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-6ec18f23.js";import{C as e}from"./CardLayout-d2005797.js";const c=r=>{const{navHeader:o,children:t}=r;return a(e,{navHeader:o,children:t})};export{c as default};
