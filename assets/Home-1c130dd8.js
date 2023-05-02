cdc3757f922f403c3ffb59f07b0256a21cfc6e18
Shahzeb Khan <mkh039@post.uit.no>
2023-05-02 20:14:11 +0200

    Deploy

---
+++ assets/Home-1c130dd8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-322a5cee.js";import{C as e}from"./CardLayout-6cad6840.js";const n=r=>{const{navHeader:o,children:a}=r;return t(e,{navHeader:o,children:a})};export{n as default};
---
+++ assets/Test_HRK_controlpanel-b2618284.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-322a5cee.js";import{C as e}from"./CardLayout-6cad6840.js";const c=r=>{const{navHeader:o,children:t}=r;return a(e,{navHeader:o,children:t})};export{c as default};
